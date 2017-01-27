dca539315b34f830809a2d04c1ecc0b9e1f7b265
andreadago <and.dago@hotmail.it>
2017-01-27 11:48:40 +0100

    Rotating door (ActorComponent OpenDoor)

---
+++ BuildingEscape/Source/BuildingEscape/OpenDoor.cpp
@@ -0,0 +1,46 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "BuildingEscape.h"
+#include "OpenDoor.h"
+
+
+// Sets default values for this component's properties
+UOpenDoor::UOpenDoor()
+{
+	// Set this component to be initialized when the game starts, and to be ticked every frame.  You can turn these features
+	// off to improve performance if you don't need them.
+	PrimaryComponentTick.bCanEverTick = true;
+
+	// ...
+}
+
+
+// Called when the game starts
+void UOpenDoor::BeginPlay()
+{
+	Super::BeginPlay();
+
+	AActor* Owner = GetOwner();
+	
+	FString Rotation = Owner->GetTransform().GetRotation().ToString();
+	UE_LOG(LogTemp, Warning, TEXT("Rotation is %s"), *Rotation);
+
+	FRotator NewRotation = FRotator(0.f, -60.f, 0.f);
+
+	Owner->SetActorRotation(NewRotation);
+	
+	Rotation = Owner->GetTransform().GetRotation().ToString();
+	UE_LOG(LogTemp, Warning, TEXT("After SetRotation: Rotation is %s"), *Rotation);
+
+}
+
+
+// Called every frame
+void UOpenDoor::TickComponent( float DeltaTime, ELevelTick TickType, FActorComponentTickFunction* ThisTickFunction )
+{
+	Super::TickComponent( DeltaTime, TickType, ThisTickFunction );
+
+	// ...
+
+}
+
